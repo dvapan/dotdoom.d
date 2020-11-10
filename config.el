21990b78f786e89189e09841100f3fd07e857a2a
Danil Apanovich <dvapan@gmail.com>
2020-11-11 00:17:55 +0800

    Установлен pyvenv и набор для sicp

--- config.el
+++ config.el
@@ -52,3 +52,8 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(use-package! ob-racket
+  :after org
+  :config
+  (append '((racket .t) (scribble . t)) org-babel-load-languages))
